567dc729ab9c6bd7aab6eb8f3777d7177e611977
Nishant Nigam <nishnig1994@gmail.com>
2023-08-23 10:24:11 +0200

    app.py with predict

--- app.py
+++ app.py
@@ -22,5 +22,14 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [x for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    print('prediction is '.format(output))
+    return render_template('home.html', prediction_text = 'prediction is {}'.format(output))
+
 if __name__ == '__main__':
     app.run(debug=True) 
